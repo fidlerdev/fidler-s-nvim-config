3c36eeef8916b8ec584ed5388d22783ed0fd9449
Benjamin <fidler@MacBook-Air-Benjamin.local>
2025-07-08 16:21:35 +0300

    colorscheme

--- lua/fidler/lazy/telescope.lua
+++ lua/fidler/lazy/telescope.lua
@@ -2,13 +2,13 @@ return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
-    require('telescope').setup({})
-    local builtin = require('telescope.builtin')
+        require('telescope').setup({})
+        local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
 		vim.keymap.set('n', ';', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-  end
+    end
 }
