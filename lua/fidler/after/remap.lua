eadf73ebcdcc3ac89d489e695559d4c6ff9ea142
fidlerdev <fidlerdev@yandex.ru>
2025-07-15 17:42:30 +0300

    update

--- lua/fidler/after/remap.lua
+++ lua/fidler/after/remap.lua
@@ -49,3 +49,22 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- Open parent directory in floating window
 vim.keymap.set("n", "<space>-", require("oil").toggle_float)
 -----------------------------------------------------------------------------------------------
+
+
+--------------------------------DAP------------------------------------------------------------
+local dap = require("dap")
+vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+-- Eval var under cursor
+vim.keymap.set("n", "<space>!", function()
+    require("dapui").eval(nil, { enter = true })
+end)
+
+vim.keymap.set("n", "<F1>", dap.continue)
+vim.keymap.set("n", "<F2>", dap.step_into)
+vim.keymap.set("n", "<F3>", dap.step_over)
+vim.keymap.set("n", "<F4>", dap.step_out)
+vim.keymap.set("n", "<F5>", dap.step_back)
+vim.keymap.set("n", "<F13>", dap.restart)
+-----------------------------------------------------------------------------------------------
--- lua/fidler/lazy/lsp.lua
+++ lua/fidler/lazy/lsp.lua
@@ -29,9 +29,8 @@ return {
 
     require('mason-lspconfig').setup({
       ensure_installed = {
-        "tsserver",
         "lua_ls",
-        "ruff"
+        "ruff",
       },
       handlers = {
         function(server_name)
